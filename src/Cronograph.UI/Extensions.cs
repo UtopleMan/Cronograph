d76ad2a844768bf7304058ea98c6977ffd1f1501
Peter <pa@truecon.dk>
2022-12-07 16:49:15 +0100

    Simplified hosting of web site

--- src/Cronograph.UI/Extensions.cs
+++ src/Cronograph.UI/Extensions.cs
@@ -6,25 +6,15 @@ using System.Reflection;
 namespace Cronograph.UI;
 public static class Extensions
 {
+    private const string physicalDir = "www";
     public static IApplicationBuilder UseCronographUI(this IApplicationBuilder applicationBuilder, string subPath = "cronograph")
     {
-        var endpointBuilder = (IEndpointRouteBuilder) applicationBuilder;
+        var endpointBuilder = (IEndpointRouteBuilder)applicationBuilder;
         var manifestEmbeddedProvider = new ManifestEmbeddedFileProvider(Assembly.GetExecutingAssembly());
-        MapFiles("www", manifestEmbeddedProvider, applicationBuilder);
+        MapFiles(physicalDir, subPath, manifestEmbeddedProvider, applicationBuilder);
 
         endpointBuilder.MapGet(subPath + "/jobs", (ICronographStore store) => store.Get().ToViewModel());
-
-        endpointBuilder.Map(subPath + "/{**:nonfile}", async ctx =>
-        {
-            var dir = manifestEmbeddedProvider.GetDirectoryContents("www").ToArray();
-            var file = dir.Where(it => it?.Name?.ToLower() == "index.html").FirstOrDefault();
-            if (file == null)
-                return;
-            var response = ctx.Response;
-            response.ContentType = GetContentType(file.Name);
-            using var fileContent = file.CreateReadStream();
-            await StreamCopyOperation.CopyToAsync(fileContent, response.Body, file.Length, CancellationToken.None);
-        });
+        endpointBuilder.Map(subPath + "/{**:nonfile}", async ctx => ctx.Response.Redirect($"{subPath}/index.html"));
         return applicationBuilder;
     }
     static string GetContentType(string file)
@@ -37,24 +27,26 @@ public static class Extensions
         return MimeTypeMap.GetMimeType(ext);
     }
 
-    static void MapFiles(string dirName, IFileProvider provider, IApplicationBuilder appBuilder)
+    static void MapFiles(string dirName, string subPath, IFileProvider provider, IApplicationBuilder appBuilder)
     {
         var folder = provider.GetDirectoryContents(dirName);
         foreach (var item in folder)
         {
             if (item.IsDirectory)
             {
-                MapFiles(dirName + "/" + item.Name, provider, appBuilder);
+                MapFiles(dirName + "/" + item.Name, subPath, provider, appBuilder);
                 continue;
             }
             string map = (dirName + "/" + item.Name);
-            map = ("/" + map).Replace("www/", "");
-            appBuilder.Map(map, app =>
+            map = ("/" + map).Replace($"{physicalDir}/", "");
+
+            Console.WriteLine("/" + subPath + map);
+            appBuilder.Map("/" + subPath + map, app =>
             {
                 var file = item;
-
                 app.Run(async cnt =>
                 {
+                    Console.WriteLine("Trying: /" + file.Name + " to physical path " + file.PhysicalPath);
                     cnt.Response.ContentType = GetContentType(map);
                     if (file.Length < 1)
                         throw new ArgumentException($"file {file.Name} does not exists");
