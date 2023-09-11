9b9f2b761a6de4ea35ea052fa1885793fa23c5ec
Peter Andersen <pa@truecon.dk>
2023-09-11 13:31:01 +0200

    Moved interface

--- src/Cronograph/ICronographStore.cs
+++ src/Cronograph/ICronographStore.cs
@@ -3,10 +3,4 @@ using System.Collections.Generic;
 
 namespace Cronograph;
 
-public interface ICronographStore
-{
-    void UpsertJob(Job job);
-    void UpsertJobRun(JobRun jobRun);
-    IReadOnlyList<Job> GetJobs();
-    IReadOnlyList<JobRun> GetJobRuns(Job job);
-}
+
